ec17f6cf7a9dea5c2658321b359d5a7ae063a870
Vipul Kelkar <vipul.kelkar@gmail.com>
2021-06-16 20:39:51 +0530

    Added tests for prompt confirmed by the user.

--- src/m365/aad/commands/app/app-role-delete.spec.ts
+++ src/m365/aad/commands/app/app-role-delete.spec.ts
@@ -9,7 +9,7 @@ import Utils from '../../../../Utils';
 import commands from '../../commands';
 const command: Command = require('./app-role-delete');
 
-describe.only(commands.APP_ROLE_DELETE, () => {
+describe(commands.APP_ROLE_DELETE, () => {
   let log: string[];
   let logger: Logger;
   let promptOptions: any;
@@ -2025,6 +2025,302 @@ describe.only(commands.APP_ROLE_DELETE, () => {
     });
   });
 
+  it('deletes an app role when the role is in enabled state and valid appObjectId, role claim and the prompt is confirmed (debug)', (done) => {
+    sinon.stub(request, 'get').callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230?$select=id,appRoles') {
+        return Promise.resolve({
+          id: '5b31c38c-2584-42f0-aa47-657fb3a84230',
+          appRoles: [
+            {
+              "allowedMemberTypes": [
+                "User"
+              ],
+              "description": "Product read",
+              "displayName": "ProductRead",
+              "id": "c4352a0a-494f-46f9-b843-479855c173a7",
+              "isEnabled": true,
+              "lang": null,
+              "origin": "Application",
+              "value": "Product.Read"
+            },
+            {
+              "allowedMemberTypes": [
+                "User"
+              ],
+              "description": "Product write",
+              "displayName": "ProductWrite",
+              "id": "54e8e043-86db-49bb-bfa8-c9c27ebdf3b6",
+              "isEnabled": true,
+              "lang": null,
+              "origin": "Application",
+              "value": "Product.Write"
+            }
+          ]
+        });
+      }
+
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    const patchStub = sinon.stub(request,'patch');
+    
+    patchStub.onFirstCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230' &&
+        opts.data &&
+        opts.data.appRoles.length === 2) {
+        const appRole = opts.data.appRoles[0];
+        if(appRole.isEnabled === false){
+          return Promise.resolve();
+        }
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    patchStub.onSecondCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230' &&
+        opts.data &&
+        opts.data.appRoles.length === 1) {
+        const appRole = opts.data.appRoles[0];
+        if(appRole.value === "Product.Write" &&
+          appRole.id === '54e8e043-86db-49bb-bfa8-c9c27ebdf3b6' &&
+          appRole.isEnabled === true){
+          return Promise.resolve();
+        }
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    Utils.restore(Cli.prompt);
+    sinon.stub(Cli, 'prompt').callsFake((options: any, cb: (result: { continue: boolean }) => void) => {
+      cb({ continue: true });
+    });
+
+    command.action(logger, {
+      options: {
+        debug: true,
+        appObjectId: '5b31c38c-2584-42f0-aa47-657fb3a84230',
+        claim: 'Product.Read',
+        confirm:true
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(typeof err, 'undefined', err?.message);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('deletes an app role when the role is in enabled state and valid appId, role name and prompt is confirmed', (done) => {
+
+    const getRequestStub = sinon.stub(request, 'get');
+
+    getRequestStub.onFirstCall().callsFake(opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/myorganization/applications?$filter=appId eq '53788d97-dc06-460c-8bd6-5cfbc7e3b0f7'&$select=id`) {
+        return Promise.resolve({
+          "value":[
+            {
+              id: '5b31c38c-2584-42f0-aa47-657fb3a84230'
+            }
+          ]
+        });
+      }
+
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    getRequestStub.onSecondCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230?$select=id,appRoles') {
+        return Promise.resolve({
+          id: '5b31c38c-2584-42f0-aa47-657fb3a84230',
+          appRoles: [
+            {
+              "allowedMemberTypes": [
+                "User"
+              ],
+              "description": "Product read",
+              "displayName": "ProductRead",
+              "id": "c4352a0a-494f-46f9-b843-479855c173a7",
+              "isEnabled": true,
+              "lang": null,
+              "origin": "Application",
+              "value": "Product.Read"
+            },
+            {
+              "allowedMemberTypes": [
+                "User"
+              ],
+              "description": "Product write",
+              "displayName": "ProductWrite",
+              "id": "54e8e043-86db-49bb-bfa8-c9c27ebdf3b6",
+              "isEnabled": true,
+              "lang": null,
+              "origin": "Application",
+              "value": "Product.Write"
+            }
+          ]
+        });
+      }
+
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    const patchStub = sinon.stub(request,'patch');
+    
+    patchStub.onFirstCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230' &&
+        opts.data &&
+        opts.data.appRoles.length === 2) {
+        const appRole = opts.data.appRoles[0];
+        if(appRole.isEnabled === false){
+          return Promise.resolve();
+        }
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    patchStub.onSecondCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230' &&
+        opts.data &&
+        opts.data.appRoles.length === 1) {
+        const appRole = opts.data.appRoles[0];
+        if(appRole.value === "Product.Write" &&
+          appRole.id === '54e8e043-86db-49bb-bfa8-c9c27ebdf3b6' &&
+          appRole.isEnabled === true){
+          return Promise.resolve();
+        }
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    Utils.restore(Cli.prompt);
+    sinon.stub(Cli, 'prompt').callsFake((options: any, cb: (result: { continue: boolean }) => void) => {
+      cb({ continue: true });
+    });
+
+    command.action(logger, {
+      options: {
+        debug: false,
+        appId: '53788d97-dc06-460c-8bd6-5cfbc7e3b0f7',
+        name: 'ProductRead',
+        confirm:true
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(typeof err, 'undefined', err?.message);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('deletes an app role when the role is in enabled state and valid appId, role id and prompt is confirmed (debug)', (done) => {
+
+    const getRequestStub = sinon.stub(request, 'get');
+
+    getRequestStub.onFirstCall().callsFake(opts => {
+      if (opts.url === `https://graph.microsoft.com/v1.0/myorganization/applications?$filter=appId eq '53788d97-dc06-460c-8bd6-5cfbc7e3b0f7'&$select=id`) {
+        return Promise.resolve({
+          "value":[
+            {
+              id: '5b31c38c-2584-42f0-aa47-657fb3a84230'
+            }
+          ]
+        });
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    getRequestStub.onSecondCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230?$select=id,appRoles') {
+        return Promise.resolve({
+          id: '5b31c38c-2584-42f0-aa47-657fb3a84230',
+          appRoles: [
+            {
+              "allowedMemberTypes": [
+                "User"
+              ],
+              "description": "Product read",
+              "displayName": "ProductRead",
+              "id": "c4352a0a-494f-46f9-b843-479855c173a7",
+              "isEnabled": true,
+              "lang": null,
+              "origin": "Application",
+              "value": "Product.Read"
+            },
+            {
+              "allowedMemberTypes": [
+                "User"
+              ],
+              "description": "Product write",
+              "displayName": "ProductWrite",
+              "id": "54e8e043-86db-49bb-bfa8-c9c27ebdf3b6",
+              "isEnabled": true,
+              "lang": null,
+              "origin": "Application",
+              "value": "Product.Write"
+            }
+          ]
+        });
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    const patchStub = sinon.stub(request,'patch');
+    
+    patchStub.onFirstCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230' &&
+        opts.data &&
+        opts.data.appRoles.length === 2) {
+        const appRole = opts.data.appRoles[0];
+        if(appRole.isEnabled === false){
+          return Promise.resolve();
+        }
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    patchStub.onSecondCall().callsFake(opts => {
+      if (opts.url === 'https://graph.microsoft.com/v1.0/myorganization/applications/5b31c38c-2584-42f0-aa47-657fb3a84230' &&
+        opts.data &&
+        opts.data.appRoles.length === 1) {
+        const appRole = opts.data.appRoles[0];
+        if(appRole.value === "Product.Write" &&
+          appRole.id === '54e8e043-86db-49bb-bfa8-c9c27ebdf3b6' &&
+          appRole.isEnabled === true){
+          return Promise.resolve();
+        }
+      }
+      return Promise.reject(`Invalid request ${JSON.stringify(opts)}`);
+    });
+
+    Utils.restore(Cli.prompt);
+    sinon.stub(Cli, 'prompt').callsFake((options: any, cb: (result: { continue: boolean }) => void) => {
+      cb({ continue: true });
+    });
+
+    command.action(logger, {
+      options: {
+        debug: true,
+        appId: '53788d97-dc06-460c-8bd6-5cfbc7e3b0f7',
+        id: 'c4352a0a-494f-46f9-b843-479855c173a7',
+        confirm:true
+      }
+    }, (err?: any) => {
+      try {
+        assert.strictEqual(typeof err, 'undefined', err?.message);
+        done();
+      }
+      catch (e) {
+        done(e);
+      }
+    });
+  });
+
   it('aborts deleting app role when prompt is not confirmed', (done) => {
     // represents the aad app get request called when the prompt is confirmed
     const patchStub = sinon.stub(request,'get');
@@ -2252,4 +2548,14 @@ describe.only(commands.APP_ROLE_DELETE, () => {
     });
     assert(containsOption);
   });
+
+
+
+
+
+ 
+
+
+
+
 });
